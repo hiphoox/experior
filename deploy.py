7b48e83f8bb6dba4264e2141743f7af821f09a25
Norberto Ortigoza <norberto.ortigoza@gmail.com>
2009-06-21 13:36:46 -0500

    Fix: We need to check if the depoyment server is using https so we create the correct location header

--- deploy.py
+++ deploy.py
@@ -4,9 +4,13 @@ class HttpsRedirect(object):
     Handles conditional HTTPS operations. If the request has https, the Location header in the redirect is replaced by an Https.
     """
     def process_response(self, request, response):
-      if request.is_secure() and request.method == 'POST':
-        print response['Location']
+      #response.__setitem__('Basura' , 'norberto')
+      if self.is_deploymentserver(request) and request.method == 'POST':
         location = 'https://' + request.META.get('SERVER_NAME') + response['Location']
         response['Location'] = location
-      
       return response
+
+    def is_deploymentserver(self, request):
+       if request.META.get('SERVER_NAME').find('webfactional') >= 0:
+         return True
+       return False

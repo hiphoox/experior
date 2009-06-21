7353b66bab638d559354b45ace1e3376a7d6b144
Norberto Ortigoza <norberto.ortigoza@gmail.com>
2009-06-21 12:24:48 -0500

    Deploy: We are setting an absolute url in the Location header

--- deploy.py
+++ deploy.py
@@ -4,8 +4,9 @@ class HttpsRedirect(object):
     Handles conditional HTTPS operations. If the request has https, the Location header in the redirect is replaced by an Https.
     """
     def process_response(self, request, response):
-      if request.is_secure() and request.method == 'POST':
-        location = response['Location'].replace('http', 'https')
+      if """request.is_secure()""" and request.method == 'POST':
+        print response['Location']
+        location = 'https://' + request.META.get('SERVER_NAME') + response['Location']
         response['Location'] = location
       
       return response

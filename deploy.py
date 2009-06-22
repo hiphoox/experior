9b8954743ed84d717600e20b82cbc00ac7d16c54
Norberto Ortigoza <norberto.ortigoza@gmail.com>
2009-06-21 22:35:31 -0500

    Fix: this hack patchs a problem with redirects(302) and https

--- deploy.py
+++ deploy.py
@@ -4,9 +4,13 @@ class HttpsRedirect(object):
     Handles conditional HTTPS operations. If the request has https, the Location header in the redirect is replaced by an Https.
     """
     def process_response(self, request, response):
-      #response.__setitem__('Basura' , 'norberto')
+      #response.__setitem__('Informacion' , response['Location'])
       if self.is_deploymentserver(request) and request.method == 'POST':
-        location = 'https://' + request.META.get('SERVER_NAME') + response['Location']
+        if self.has_relative_reference(response):
+          location = self.create_url_with_relative(request, response)
+        else:
+          location = self.create_url(request, response)
+          
         response['Location'] = location
       return response
 
@@ -14,3 +18,25 @@ class HttpsRedirect(object):
        if request.META.get('SERVER_NAME').find('webfactional') >= 0:
          return True
        return False
+
+    def has_relative_reference(self, response):
+        if response['Location'].find('../') >= 0:
+          return True
+        return False
+
+    def create_url(self, request, response):
+        path = request.META.get('HTTP_REFERER').replace('https://experior.webfactional.com','')
+        url = 'https://' + request.META.get('SERVER_NAME') + path
+        return url
+   
+    def create_url_with_relative(self, request, response):
+        path = request.META.get('HTTP_REFERER').replace('https://experior.webfactional.com','')
+        elements = path.strip('/').split('/')
+        path = '/'.join(elements[:-1])
+        relative = response['Location']
+        commands = relative.strip('/').split('/')
+        commands = '/'.join(commands[1:]) + '/'
+        url = 'https://' + request.META.get('SERVER_NAME') + '/' + path + '/'
+        if commands != '/':
+           url = url + commands
+        return url

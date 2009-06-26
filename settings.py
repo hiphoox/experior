4bab4f9f457b12303332190cbccf27f7dd29ff0f
Norberto Ortigoza <norberto.ortigoza@gmail.com>
2009-06-26 15:54:57 -0500

    Conf: Adding deployment support

--- settings.py
+++ settings.py
@@ -10,8 +10,10 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+SETTINGS_DIR = os.path.abspath(os.path.dirname(__file__)) 
+
 DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = os.path.join(os.path.dirname(__file__), 'experior.db') # Or path to database file if using sqlite3.
+DATABASE_NAME = os.path.join(SETTINGS_DIR, 'experior.db') # Or path to database file if using sqlite3.
 DATABASE_USER = ''             # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
@@ -71,8 +73,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(os.path.dirname(__file__), 'templates'),
-    os.path.join(os.path.dirname(__file__), 'timetracking/templates'),
+    os.path.join(SETTINGS_DIR, 'templates'),
+    os.path.join(SETTINGS_DIR, 'timetracking/templates'),
 )
 
 INSTALLED_APPS = (
@@ -84,3 +86,8 @@ INSTALLED_APPS = (
     'django_extensions',
     'experior.timetracking',
 )
+
+try: 
+    from local_settings import * 
+except ImportError: 
+    pass

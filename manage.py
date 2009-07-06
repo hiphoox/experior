42107ab0c7d2f8b2763b0e1a36c006aef5887aae
Norberto Ortigoza <norberto.ortigoza@gmail.com>
2009-07-05 23:31:06 -0500

    Ref: Removing duplication and cleaning code

--- manage.py
+++ manage.py
@@ -7,14 +7,16 @@ except ImportError:
     sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
     sys.exit(1)
 
-#Custome operations
+# Custome operations
 # By Norberto Ortigoza
 import os, sys 
 deployment_server = "experior@experior.webfactional.com"
 repository_path = "/home/experior/webapps/django_trunk/experior/"
 ssh_key_file = "/home/experior/.ssh/id_rsa"
-git_command = "/home/experior/bin/git"
-build_command = "manage.py rebuild_db; restart"
+git_command = "git pull"
+rebuild_remote_command = "manage.py rebuild_db; restart"
+# The -t option allows direct interaction with the command's execution (passphrase entry)
+remote_initialitation = "ssh -t " + deployment_server + " 'source .bash_profile; cd " +  repository_path + "; eval `ssh-agent`; ssh-add " + ssh_key_file  +  "; "
 
 if len(sys.argv)  > 1:
   operation = sys.argv[1]
@@ -22,18 +24,18 @@ else:
   operation = ''
   
 if operation == "rebuild_db":
-  print "Reseting DB:"
+  print "Rebuilding local DB:"
   os.system("rm experior.db")
   os.system("manage.py syncdb")
 
 elif operation == "deploy": 
+  print "Deploying local DB:"
   os.system("git push")
-  #The -t option allows direct interaction with the command's execution (passphrase entry)
-  os.system("ssh -t " + deployment_server + " 'cd " +  repository_path + "; eval `ssh-agent`; ssh-add " + ssh_key_file  +  "; " + git_command  + " pull'")
+  os.system(remote_initialitation + git_command + "'")
 
-elif operation == "rebuild_remote_db": 
-  #The -t option allows direct interaction with the command's execution (passphrase entry)
-  os.system("ssh -t " + deployment_server + " 'cd " +  repository_path + "; eval `ssh-agent`; ssh-add " + ssh_key_file  +  "; " + build_command + "'")
+elif operation == "rebuild_remote": 
+  print "Rebuilding remote server:"
+  os.system(remote_initialitation + rebuild_remote_command + "'")
 
 else:
   if __name__ == "__main__":
